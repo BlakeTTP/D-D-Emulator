c875e3b002098c03bb90bd1780d757f08f5cf579
BlakeTTP <summersolsticemayhem@gmail.com>
2025-08-04 12:35:52 -0400

    Tempbeginning

--- Emulator.py
+++ Emulator.py
@@ -1,10 +1,25 @@
 import random
-#random.randint(1, 5)
+#random.randint(1, 6)
+#random.randint(1, 20)
 #comment
 
-print("beginningpart")
-input("Press Enter to continue.")
-print("begin2")
-stat1 = int (input("Enter a number: "))
+input("(Whenever you don't need to enter anything in, then just press ENTER to continue.)")
+input("First, to begin your story, we need to configure your stats.")
+input("There's six to configure.")
+input("Strength(STR): Determines how physically strong you are.")
+input("Dexterity(DEX): Determines your flexibility, dodging, and sneaking ability.")
+input("Constitution(CON): Determines how much your body can physically take, mainly health.")
+input("Wisdom(WIS): Determines how wise/conventional wisdom you have, or 'common sense'.")
+input("Intelligence(INT): Determines how much you know, or 'book knowledge'.")
+input("Charisma(CHA): Determines your social skills.")
+input("Everytime you make a decision, a die is rolled to see how effective it is.")
+input("These stats are modifiers that make you better or worse at certain skills.")
+input("The array you're allowed to use is -1, 0, 1, 1, 2, and 3.")
+STR = int (input("Enter your strength: "))
+DEX = int (input("Enter your dexterity: "))
+CON = int (input("Enter your constitution: "))
+WIS = int (input("Enter your wisdom: "))
+INT = int (input("Enter your intelligence: "))
+CHA = int (input("Enter your charisma: "))
 
 
