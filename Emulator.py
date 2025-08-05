5083e9a94453860fc9f7fe9a4af2b6356e4c4dde
BlakeTTP <summersolsticemayhem@gmail.com>
2025-08-04 20:33:18 -0400

    with STR roll now!

--- Emulator.py
+++ Emulator.py
@@ -1,15 +1,16 @@
 import random
 #random.randint(1, 6)
 #random.randint(1, 20)
-#comment
 
+#beginning part/decription of skills
 input("(Whenever you don't need to enter anything in, then just press ENTER to continue.)")
+input("(As well, answer everything in all lowercase, and no typos.)")
 input("First, to begin your story, we need to configure your stats.")
 input("There's six to configure.")
 input("Strength(STR): Determines how physically strong you are.")
 input("Dexterity(DEX): Determines your flexibility, dodging, and sneaking ability.")
 input("Constitution(CON): Determines how much your body can physically take, mainly health.")
-input("Wisdom(WIS): Determines how wise/conventional wisdom you have, or 'common sense'.")
+input("Wisdom(WIS): Determines how wise you are/how much conventional wisdom you have, or 'common sense'.")
 input("Intelligence(INT): Determines how much you know, or 'book knowledge'.")
 input("Charisma(CHA): Determines your social skills.")
 input("Everytime you make a decision, a die is rolled to see how effective it is.")
@@ -20,7 +21,38 @@ DEX = int (input("Enter your dexterity: "))
 CON = int (input("Enter your constitution: "))
 WIS = int (input("Enter your wisdom: "))
 INT = int (input("Enter your intelligence: "))
-#CHA = int (input("Enter your charisma: "))
+CHA = int (input("Enter your charisma: "))
 
+#STR roll function
+def rollSTR():
+    roll = random.randint(1,20)
+    print("You rolled a "+str(roll)+"!")
+    check = roll+STR
+    if STR > 0:
+        print("+"+str(STR)+" = "+str(check)+" overall!")
+        return check
+    if STR < 0:
+        print(str(STR)+" = "+str(check)+" overall!")
+        return check
+    
+#continuation
+input("Now, to begin your journey.")
+answer1 = input("testcheck1: enter yes no or maybe: ")
+if answer1 == "yes":
+    input("you said yes!")
+    a1 = 1
+    input("since you said yes, this thing happens! roll for strength!")
+    check = rollSTR()
+    if check > 9:
+        input("you passed the check! insert good job here!")
+    if check < 9:
+        input("you failed the check! rip")
 
+if answer1 == "no":
+    input("you said no!")
+    a1 = 2
+
+if answer1 == "maybe":
+    input("you said maybe!")
+    a1 = 3
 
