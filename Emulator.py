258be17f676b0454899c75232207b151f0fbfe03
BlakeTTP <summersolsticemayhem@gmail.com>
2025-08-06 22:27:30 -0400

    beginning choices up!

--- Emulator.py
+++ Emulator.py
@@ -149,6 +149,7 @@ def beginning():
 a1bashfail = False
 a1lockfail = False
 a1callfail = False
+check = 0
 
 #beginning choice
 def beginningchoice():
@@ -158,10 +159,20 @@ def beginningchoice():
         print("Bashing the door down - 'bash'")
     elif role == "mage":
          print("\nUsing magic to grab your tome - 'cast' (This will use up one of your three spells)")
-    elif role == "rogue" and a1bashfail == False:
+    elif role == "rogue" and a1lockfail == False:
         print("Lockpicking the door - 'lock'")
     if a1callfail == False:
         print("Calling out for a guard to trick - 'call'")
+    if role == "warrior" and a1bashfail == True and a1callfail == True:
+        input("You've already tried bashing the door, and tricking the guard didn't work.")
+        input("You slump down in your cell, awaiting rescue.")
+        input("GAME OVER! Try again!")
+        exit()
+    if role == "rogue" and a1lockfail == True and a1callfail == True:
+        input("You've already tried lockpicking the door, and tricking the guard didn't work.")
+        input("You slump down in your cell, awaiting rescue.")
+        input("GAME OVER! Try again!")
+        exit()
     a1 = input("\nWhat do you want to do? Enter: ")
     return a1
 
@@ -172,12 +183,14 @@ def beginningchoiceif():
     global a1bashfail
     global a1lockfail
     global a1callfail
+    global check
 
     if nexta1 == "bash" and a1bashfail == False:
         input("You attempt to bash the door down! Strength check!")
         check = rollSTR()
         if check >= 6:
             input("You passed the check! The door's hinges creak loudly as the rotting wood crashes down.")
+            input("You confidently step outside, resoundly stepping on the shards of the door.")
             return "abash"
         elif check < 6:
             input("You failed the check! Try something else!")
@@ -202,7 +215,7 @@ def beginningchoiceif():
         if spell_choice == "1" or spell_choice == "firebolt" and "Firebolt" not in used_spells:
             used_spells.append("Firebolt")
             input("You cast Firebolt! The door bursts into flames and collapses.")
-            input("As the flames flicker, you carefully step over the decimated door to grab your stuff, when-")
+            input("As the flames flicker, you carefully step over the decimated door.")
             return "afirebolt"
 
         elif spell_choice == "2" or spell_choice == "frost shield" and "Frost Shield" not in used_spells:
@@ -211,7 +224,7 @@ def beginningchoiceif():
             input("...")
             input("Since you're trying to get out and nothing's trying to get in, there's nothing it can really do.")
             input("Try again!")
-            print(used_spells)
+            #print(used_spells) Btw the used spells doesn't work for some weird reason - to do later
             nexta1 == beginningchoice()
             beginningchoiceif()
 
@@ -225,25 +238,69 @@ def beginningchoiceif():
             nexta1 == beginningchoice()
             beginningchoiceif()
     #rogue
-    elif nexta1 == "lock" :
-        input("You attempt to pick the lock... ")
+    elif nexta1 == "lock" and a1lockfail == False:
+        input("You take out your hair pin, letting your hair down for a bit.")
+        input("You crouch, checking if there's any guards watching you.")
+        input("All clear.")
+        input("You attempt to pick the lock...")
         print("Dexterity check!!!")
         check = rollDEX()
         if check >= 7:
-            input("Sucsess! The lock clicks open. You silently slip into the next stage of escape")
+            input("Success! The lock clicks open. You silently slip out of your cell.")
         elif check < 7:
-            input("You failed the check!\n GAME OVER!!!")
-            exit()
-    #general
-    elif nexta1 == "call":
-        print("You knock on the door, pretending to be scared.")
-        print("Footsteps approach quickly...")
-        print("As the guard opens the door, your hidden magic rune ignites!")
-        input("The guard is thrown back, unconscious. The way is clear.")
+            input("As you attempt to push it in, you push too far in the wrong direction as you hear the wrong click.")
+            input("You retract the pin, now broken in two.")
+            input("You failed the check! Try something else!")
+            a1lockfail = True
+            nexta1 == beginningchoice()
+            beginningchoiceif()
+    elif nexta1 == "lock" and a1lockfail == True:
+        print("You already failed that option! Try a different one.") 
+        nexta1 = beginningchoice()
+        beginningchoiceif()
+
+    #general - call
+    elif nexta1 == "call" and a1callfail == False:
+        print("You cry out, pretending to be scared.")
+        input("Footsteps approach quickly...")
+        input("A hulking guard looms over you, but any intimidation factor is lost alongside the crumbs falling from his face, as he takes another bite into his sandwich.")
+        input("'Hey, damsel.'")
+        input("(What does he mean by THAT?)")
+        input("Well, anyway.")
+        input("Roll to attempt to trick the guard! Charisma check!")
+        check = rollCHA()
+        if check >= 6:
+            input("You passed the check! You're pretty sure he thinks you're 'defenseless' as a 'damsel in distress'.")
+            input("You lean into the act. 'Oh no, I don't know what to do anymore! The wound on my leg might get infected, if only I could see a doctor!'")
+            input("The guard rolls his eyes. 'What, do you want someone to look at it or something?")
+            print("It's not like you're going to die before the hero gets here.'")
+            input("(So they're using you as bait, huh...) 'I'm not sure I can make it!'")
+            input("(For dramatic effect, as you sit on the cold murky ground, you clutch your leg to your chest.)")
+            input("The guard sighs. 'Damn damsel already making my life difficult...' He mutters to himself.")
+            input("He starts getting his keys out for the door. As the key slots in, the door swings open as you lunge for him!")
+            input("'Wh- I thought you were injured!' he yells in the struggle, as you press your boot against his head as you grasp for the keys.")
+            input("Got them!")
+            input("As you kick the guard one final time, he falls unconcious as you brush yourself off. Man. You can't believe how easy that was.")
+            input("The sandwich remains disgarded as you step out the cell.")
+            return "acall"
+        elif check < 6:
+            input("You decide to act defenseless. 'Ow, my knee really hurts!'")
+            input("He raises his eyebrows at you disbelievingly. 'Yeah, sure, sure.'")
+            input("He leans slightly towards the cell. 'Cry about it.'")
+            input("The guard turns around, and right as he's about to walk away, he stops.")
+            input("'Don't call me unless there's something real.' He continues walking away, along with the keys.")
+            input("You failed the check! Try something else.")
+            a1callfail = True
+            nexta1 = beginningchoice()
+            beginningchoiceif() 
+    elif nexta1 == "call" and a1callfail == True:
+        print("You already failed that option! Try a different one.") 
+        nexta1 = beginningchoice()
+        beginningchoiceif()
 
 #beginning()
 nexta1 = beginningchoice()
-a = beginningchoiceif()
+a1choice = beginningchoiceif()
 #successful choices: afirebolt, abash, acall, alock, ateleport
 
 
