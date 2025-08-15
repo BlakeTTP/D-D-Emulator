edcd92eab31b7c45a5932ad47c9e3fb7eb1cde6e
YachiAmin <yachiamin0704@gmail.com>
2025-08-15 17:38:20 +0530

    Update Emulator.py

--- Emulator.py
+++ Emulator.py
@@ -19,21 +19,23 @@ to-do:
 autoStatConfig = False
 
 #beginning part/decription of skills
-input("\033[36mWELCOME 🙏, ADVENTURER, TO OUR REALM!!!\033[0m\n (Whenever you don't need to enter anything in, then just press ENTER to continue.)")
+input("\033[1;34mWELCOME 🙏, ADVENTURER, TO OUR REALM!!!\033[0m\n (Whenever you don't need to enter anything in, then just press ENTER to continue.)")
 
 while True:
-     inputDebugMode = input("Turn on debug mode? Enter \033[32mTrue\033[0m or \033[31mFalse\033[0m: ")
+     inputDebugMode = input("Turn on debug mode? \033[1mEnter\033[0m \033[32mTrue\033[0m or \033[31mFalse\033[0m: ")
      if inputDebugMode.lower() == "false":
          debugMode = False
+         autoStatConfig = False 
          break
      elif inputDebugMode.lower() == "true":
          debugMode = True
          break
      else:
-         print("Invalid input. Please enter true or false!!")
+         print("Invalid input. Please Enter either TRUE or FALSE :")
+
 while True:         
     if debugMode:
-        inputAutoStatConfig = input("Put in stats automatically? Enter True or False: ")
+        inputAutoStatConfig = input("Put in stats automatically? \033[1mEnter\033[0m \033[32mTrue\033[0m or \033[31mFalse\033[0m: ")
         if inputAutoStatConfig.lower() == "true":
             autoStatConfig = True
             break
@@ -41,46 +43,48 @@ while True:
             autoStatConfig = False
             break
         else:
-            print("Invalid input. Please enter true or false!!")
-input("Now, let's begin your journey.\n ")
+            print("Invalid input. Please Enter TRUE or False")
+    else:
+        break
+input("\n\n\033[92mNow, let's begin your journey.\033[0m\n ")
 
 def rolechoice():
     global role
-    print("\nChoose your role: warrior, mage or rogue.")
-    role = input("Enter your role: ").strip().lower() 
-    #based on the roles
-   # if role == "warrior" or role == "rogue" or role == "mage":
-        
-    if role=="warrior":
-        print("Great Choice!!!")
-        print("You are a fearless Warrior, clad in heavy armor, with shield and sword in skilled hands.") #", marching toward the Fortress"
-        print("(+1 to Strength)")
-    elif role=="mage":
-        print("Great Choice!!!")
-        #print("You are a master Thief, who is quick, clever, and silent, with hands skilled in locks and a mind sharp as any blade.")
-        print("You are a Mage, an apprentice learning the art of the magic that flows through your world, knowledge heavy as your tome.")
-        print("(+1 to Intelligence)")
-    elif role=="rogue":
-        print("Great Choice!!!")
-        print("You are a Rogue, who is silent but deadly with a sharp dagger no matter where you stand, and always four steps ahead.")
-        print("(+1 to Dexterity)")
-    else:
-        input("Invalid option! Try again.")
-        role = rolechoice()
+    while True:
+        print("\n\033[1mChoose your role:\033[0m \033[33mWarrior,\033[36m Mage \033[0mor \033[35mRogue.\033[0m")
+        role = input("\033[1mEnter your role:\033[0m ").strip().lower()
+
+        if role == "warrior":
+            print("Great Choice!!!")
+            print("You are a fearless Warrior🤺, clad in heavy armor 𐂫, with shield 🛡️ and sword 🗡️ in skilled hands.")
+            print("(\033[32m+1 to Strength\033[0m)")
+            break
+        elif role == "mage":
+            print("Great Choice!!!")
+            print("You are a Mage, an apprentice learning the art of the magic that flows through your world, knowledge heavy as your tome.")
+            print("(\033[32m+1 to Intelligence\033[0m)")
+            break
+        elif role == "rogue":
+            print("Great Choice!!!")
+            print("You are a Rogue, who is silent but deadly with a sharp dagger no matter where you stand, and always four steps ahead.")
+            print("(\033[32m+1 to Dexterity\033[0m)")
+            break
+        else:
+            print("\033[31mInvalid option! Try again.\033[0m")
     return role
 
 role = rolechoice()
 
 #stat configuration
 def introStatConfig():
-    input("First, to begin your story, we need to configure your stats.")
-    input("Whatever class you choose already added +1 to one of them, which stacks with whatever you choose as your array.")
-    input("While you can choose otherwise, it's recommended to have your main stat you use for your class be high for class specific options.")
-    input("There's six to configure.")
-    print("\nStrength(STR): Determines how physically strong you are.\nDexterity(DEX): Determines your flexibility, dodging, and sneaking ability.\nConstitution(CON): Determines how much your body can physically take, mainly health.")
-    print("Wisdom(WIS): Determines how wise you are/how much conventional wisdom you have, or 'common sense'.\nIntelligence(INT): Determines how much you know, or 'book knowledge'.\nCharisma(CHA): Determines your social skills.")
-    input("\nEverytime you make a decision, a die is rolled to see how effective it is.\nThese stats are modifiers that make you better or worse at certain skills.")
-    input("You possess a unique set of trait values to distribute among your skills.\nUse each value wisely, once assigned, it cannot be used again [-1, 0, 1, 1, 2, and 2.]")
+    input("\033[1mFirst, to begin your story, we need to configure your stats.\033[0m")
+    input("Whatever class you choose already added \033[32m+1\033[0m to one of them, which stacks with whatever you choose as your array.")
+    input("While you can choose otherwise, it's \033[36mrecommended\033[0m to have your main stat you use for your class be high for class specific options.")
+    input("There's \033[1;33msix\033[0m to configure.")
+    print("\n\033[33mStrength(STR)\033[0m: \033[36mDetermines how physically strong you are.\033[0m\n\033[33mDexterity(DEX)\033[0m: \033[36mDetermines your flexibility, dodging, and sneaking ability.\033[0m\n\033[33mConstitution(CON)\033[0m: \033[36mDetermines how much your body can physically take, mainly health.\033[0m")
+    print("\033[33mWisdom(WIS)\033[0m: \033[36mDetermines how wise you are/how much conventional wisdom you have, or 'common sense'.\033[0m\n\033[33mIntelligence(INT)\033[0m: \033[36mDetermines how much you know, or 'book knowledge'.\033[0m\n\033[33mCharisma(CHA)\033[0m: \033[36mDetermines your social skills.\033[0m")
+    input("\nEverytime you make a decision, a \033[1;33mdie\033[0m is rolled to see how effective it is.\nThese stats are modifiers that make you better or worse at certain skills.")
+    input("You possess a unique set of trait values to distribute among your skills.\nUse each value wisely, once assigned, it cannot be used again \033[1;32m[-1, 0, 1, 1, 2, and 2.]\033[0m")
 
 char_stats = Stats()
 if autoStatConfig == False:
@@ -91,11 +95,11 @@ if autoStatConfig == True:
     char_stats.autoStats()
 
 #class based addition to stats
-    if role == "warrior":
+    if role == "Warrior":
         char_stats.STR = char_stats.STR+1
-    elif role == "mage":
+    elif role == "Mage":
         char_stats.INT = char_stats.INT+1
-    elif role == "rogue":
+    elif role == "Rogue":
         char_stats.DEX = char_stats.DEX+1
 #beginning
 def beginning(): #change pacing, add more description to environment
@@ -103,13 +107,13 @@ def beginning(): #change pacing, add more description to environment
     input("What, did someone hit y- oh. Someone did.")
     input("First things first.")
     input("You were in the village, training like usual.")
-    if role == "warrior":
+    if role == "Warrior":
         input("Under the watchful eye of your mentor, you were attempting a new battle stance against some test dummies.")
         input("Exhausted, having sheathed your weapon away, you decided a break would renew your energy.")
-    elif role == "mage":
+    elif role == "Mage":
         input("Spellbook sprawled uncomfortably close to the edge of your desk, arms raised, you were practicing a new spell.")
         input("You weren't having much luck with it. Deciding on a break, you slotted it within your robe as you opened your door.")
-    elif role == "rogue":
+    elif role == "Rogue":
         input("Opened locks were strewn about as you sat at the center of them crosslegged, them radiating outwards from you like a fractal.")
         input("As you finally picked the last one, marking the 50th, you decided a celebratory break was in order.")
     input("Shielding your eyes from the sunlight, you went for a walk towards the nearest bakery for a fresh loaf of bread.")
@@ -136,3 +140,6 @@ if officiala1 == "acall" or officiala1 == "alock" or officiala1 == "ateleport":
     choiceB2 = B2(char_stats, debugMode, role, officiala1, choiceA1.a1BashFail, choiceA1.a1LockFail, choiceA1.a1CallFail)
     nextb2 = choiceB2.b2choice()
     officialb2 = choiceB2.b2choiceif()
+
+
+
