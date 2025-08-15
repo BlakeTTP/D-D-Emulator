06fe167a6f290d7130ebf55861b8bc49d24e79bd
YachiAmin <yachiamin0704@gmail.com>
2025-08-15 17:29:18 +0530

    Update Dice.py

--- Dice.py
+++ Dice.py
@@ -9,13 +9,46 @@ class Stats:
     WIS: int
     CHA: int
 
+    
+    def __init__(self):
+        self.STR = 0
+        self.DEX = 0
+        self.CON = 0
+        self.WIS = 0
+        self.INT = 0
+        self.CHA = 0
+
     def inputStats(self):
-        self.STR = int (input("\nEnter your strength: "))
-        self.DEX = int (input("Enter your dexterity: "))
-        self.CON = int (input("Enter your constitution: "))
-        self.WIS = int (input("Enter your wisdom: "))
-        self.INT = int (input("Enter your intelligence: "))
-        self.CHA = int (input("Enter your charisma: "))
+        available_values = [-1, 0, 1, 1, 2, 2]
+        print("\033[36mYou possess a unique set of trait values to distribute among your skills.\nUse each value wisely, once assigned, it cannot be used again [-1, 0, 1, 1, 2, and 2.]\033[0m")
+        
+        for stat in ["Strength (STR)", "Dexterity (DEX)", "Constitution (CON)", "Wisdom (WIS)", "Intelligence (INT)", "Charisma (CHA)"]:
+            while True:
+                print(f"\n\033[33mAvailable values:\033[0m {available_values}")
+                try:
+                    val = int(input(f"\033[36mAssign a value to {stat}: \033[0m"))
+                except ValueError:
+                    print("\033[31mInvalid input! Please enter a number.\033[0m")
+                    continue
+
+                if val in available_values:
+                    if "Strength" in stat:
+                        self.STR = val
+                    elif "Dexterity" in stat:
+                        self.DEX = val
+                    elif "Constitution" in stat:
+                        self.CON = val
+                    elif "Wisdom" in stat:
+                        self.WIS = val
+                    elif "Intelligence" in stat:
+                        self.INT = val
+                    elif "Charisma" in stat:
+                        self.CHA = val
+                    available_values.remove(val)
+                    break
+                else:
+                    print("\033[31mInvalid choice! Pick from the available values only.\033[0m")
+       
     def autoStats(self):
         self.STR = 1
         self.DEX = 1
